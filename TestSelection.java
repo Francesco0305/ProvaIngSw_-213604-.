dba810225d14368fc1608c09c243e06f6fdabf16
Francesco0305 <64034217+Francesco0305@users.noreply.github.com>
2022-10-28 17:08:26 +0200

    Upload test
    Aggiunti 2 test per le classi già esistenti
    update test per la classe che converte

--- TestSelection.java
+++ TestSelection.java
@@ -9,35 +9,30 @@ import org.junit.Test;
 
 import mainApp.FunnyAlgorithms;
 
-public class TestBinary {
-
+public class TestSelection {
 	FunnyAlgorithms f;
 	int numeri1[] = {1,2,3,4,6};
 	
+
 	static DateTime time;
 	
 	@BeforeClass
-	static public void setTime() throws Exception {
+	public static void setUpBeforeClass() throws Exception {
 		time=new DateTime();
-		
 	}
 
 	@Before
 	public void setUp() throws Exception {
 		f = new FunnyAlgorithms();
-		
 	}
 
 	@Test
-	public void testTargetOut() {
-		assertNotEquals("target doesn't exist in the array " + time.toLocalTime() ,-1, f.binarySearch(numeri1, 10));
+	public void testOutLimite() {
+		f.selectionSort(numeri1, 2);
 	}
-	
-	
+
 	@Test
-	public void testNumberAllOk() {
-		assertNotEquals(-1, f.binarySearch(numeri1, 2));
+	public void testAllOk() {
+		f.selectionSort(numeri1, 0);
 	}
-
-
 }
